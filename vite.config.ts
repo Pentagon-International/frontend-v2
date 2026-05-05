8818e1072baed6532dcd6032874b25f99b536a93
manojchandrasekaran <manoj.chandrasekaran.08@gmail.com>
2026-05-05 18:14:10 +0530

    Routing changes since previous merge with uat branch

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   // Dev/prod default: serve from site root (/)
-  base: '/uat/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
