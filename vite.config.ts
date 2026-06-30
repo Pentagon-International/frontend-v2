f14545efa8376586262ef626043139b3c7d2504a
Padmesh01 <padmesh@pentagonindia.net>
2026-06-30 21:05:26 +0530

    Vite config manual chunk generation removed...

--- vite.config.ts
+++ vite.config.ts
@@ -25,40 +25,8 @@ export default defineConfig({
     cors: true,
   },
   build: {
-    outDir: "dist",
     minify: "esbuild",
     sourcemap: false,
-    target: "es2020",
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (!id.includes("node_modules")) return;
-
-          if (
-            id.includes("plotly.js-basic-dist") ||
-            id.includes("react-plotly.js")
-          ) {
-            return "plotly";
-          }
-          if (id.includes("pdfjs-dist")) {
-            return "pdfjs";
-          }
-          if (id.includes("echarts") || id.includes("zrender")) {
-            return "echarts";
-          }
-          if (id.includes("@mantine")) {
-            return "mantine";
-          }
-          if (
-            id.includes("react-dom") ||
-            id.includes("react-router") ||
-            id.includes("/react/")
-          ) {
-            return "react-vendor";
-          }
-        },
-      },
-    },
   },
   // Define template name and metadata
   define: {
