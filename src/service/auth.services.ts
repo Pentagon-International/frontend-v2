25a96807254b8e2368235cb968f6dda7b6393328
Shankarmoorthy391 <shankar.moorthy@pentagonindia.net>
2026-08-25 15:18:44 +0530

    sso implementation in login

--- src/service/auth.services.ts
+++ src/service/auth.services.ts
@@ -1,33 +1,20 @@
 import { apiCallProtected } from "../api/axios";
 import { URL } from "../api/serverUrls";
 
-export interface LoginFormData {
+export interface PasswordLoginData {
   pulse_id: string;
   full_name: string;
   password: string;
 }
 
-const authToken = import.meta.env.VITE_API_AUTH_TOKEN;
+export interface AzureLoginData {
+  login_type: "azure";
+  id_token: string;
+}
 
-// export const login = (data: LoginFormData) => {
-//   return new Promise((resolve, reject) => {
-//     apiCallProtected
-//       .post(URL.loginUser, data, {
-//         headers:{
-//           Authorization: `Token ${authToken}`
-//         }
-//       })
-//       .then((response) => {
-//         resolve(response);
-//       })
-//       .catch((error) => {
-//         reject(error);
-//       });
-//   });
-// };
+export type LoginFormData = PasswordLoginData | AzureLoginData;
+
 export const login = (data: LoginFormData) => {
-  // console.log("login payload-", data);
-
   return new Promise((resolve, reject) => {
     apiCallProtected
       .post(URL.loginUser, data)
