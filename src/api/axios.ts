4dc5735041c0848b863dcaf1e875eab525f6ebf4
Padmesh01 <padmesh@pentagonindia.net>
2026-09-16 17:56:28 +0530

    update: backend request add with X-Client-Type for identifying this as web requests..

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,9 +3,10 @@ import { URL } from "./serverUrls";
 
 export const apiCall = axios.create({
   baseURL: URL.base,
+  headers: { "X-Client-Type": "web" },
 });
 
 export const apiCallProtected = axios.create({
   baseURL: URL.base,
-  headers: { "Content-Type": "application/json" },
+  headers: { "Content-Type": "application/json", "X-Client-Type": "web" },
 });
